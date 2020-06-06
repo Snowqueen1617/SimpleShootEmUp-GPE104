918cb81b51f9a95a3ff09eb29dda71fb6a02808c
Snowqueen1617 <65308047+Snowqueen1617@users.noreply.github.com>
2020-06-05 22:59:16 -0700

    Milestone1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score;
+    
+    public GameObject player;
+
+    void Awake() //happens before start function
+    {
+        if(instance == null) //if nothing has been assigned to instance yet, assign
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); //hold on to the data of this object when loading a new scene
+        }
+        else
+        {
+            Destroy(this.gameObject); //there is, destroy the game object
+            UnityEngine.Debug.LogError("[GameManager] Attempted to create a second game manager: " + this.gameObject.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform tf;
+    public float turnSpeed = 100f; //Degrees per second
+    public float moveSpeed = 5f; // Units per second
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.player = this.gameObject;
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frameS
+    void Update()
+    {
+        HandleRotation();
+     
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.Self);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    public void Shoot()
+    {
+        //TODO: Implement Shooting
+        UnityEngine.Debug.Log("Shooting not implemented yet");
+    }
+
+    public void HandleRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed * Time.deltaTime);
+        }
+    }
+}
