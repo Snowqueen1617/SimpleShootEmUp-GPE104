af05e8d2ff3cefcbae818d402399fd0aa4c918b1
Sammi Wilcox <samwilco@uat.edu>
2020-09-25 22:33:15 -0700

    FINAL

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip Pew;
+    public AudioClip Begin;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.clip = Pew;
+            audioSource.Play();
+        }
+
+        //if(Input.GetKeyDown(KeyCode.P))
+        //{
+        //    audioSource.clip = Begin;
+        //    audioSource.Play();
+        //}
+    }
+
+
+}
